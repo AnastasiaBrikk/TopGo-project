b647f91a09776e01f9b3f96ec082e0a498a6eaee
Anastasia <nastya.proskuryakova.98@mail.ru>
2024-05-24 00:22:42 +0300

    main page adaptive for tablen completed

--- js/app.js
+++ js/app.js
@@ -15,18 +15,23 @@ $(document).ready(function () {
     // centerMode: true,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-        },
+          slidesToScroll: 1
+        }
       },
       {
-        breakpoint: 480,
+        breakpoint: 600,
         settings: {
           slidesToShow: 1,
-        },
+          slidesToScroll: 1
+        }
       },
-    ],
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
 
